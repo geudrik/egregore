0f3926b842f17e8ae77a404e027079a0c1e77496
Pat Litke <2020115+geudrik@users.noreply.github.com>
2024-05-17 20:34:46 -0400

    Plum Tag History Listing endpoint

--- app/api/routes/audit.py
+++ app/api/routes/audit.py
@@ -1,29 +1,12 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
-
-from app.models.pagination import PaginatedTagHistoryList, SortingArgs, FilteringArgs, PaginationArgs
-from app.models.tag import TagHistory
-from app.service.factory import get_tag_history_service
-from app.service.tag_history import TagHistoryService
+from fastapi import APIRouter
 
 audit_router = APIRouter(prefix="/audit", tags=["Audit"])
 
 
 @audit_router.get("/tags/{tag_id}", tags=["Paginated"])
-async def list_the_audit_record_for_the_supplied_tag(
-    tag_id: UUID,
-    history_service: TagHistoryService = Depends(get_tag_history_service),
-    pagination=Depends(PaginationArgs),
-    filtering=Depends(FilteringArgs),
-    sorting=Depends(SortingArgs),
-) -> PaginatedTagHistoryList:
-    query = {"term": {"id": str(tag_id)}}
-    count, limit, offset, res = await history_service.list(
-        pagination, filtering, sorting, filter_deleted=False, extra_filter=query
-    )
-    ret = [TagHistory(**i["_source"]) for i in res]
-    return PaginatedTagHistoryList(limit=limit, offset=offset, total=count, items=ret)
+async def list_the_audit_record_for_the_supplied_tag(tag_id: UUID): ...
 
 
 @audit_router.get("/users/{username}", tags=["Paginated"])
--- app/api/routes/tag_history.py
+++ app/api/routes/tag_history.py
@@ -1,9 +1,26 @@
 from uuid import UUID
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from app.models.pagination import PaginationArgs, FilteringArgs, SortingArgs, PaginatedTagHistoryList
+from app.models.tag import TagHistory
+from app.service.factory import get_tag_history_service
+from app.service.tag_history import TagHistoryService
 
 tag_history_router = APIRouter(prefix="/tags/history", tags=["Tag History"])
 
 
 @tag_history_router.get("/{tag_id}", tags=["Paginated"])
-def list_all_historical_changes_for_a_tag(tag_id: UUID): ...
+async def list_all_historical_changes_for_a_tag(
+    tag_id: UUID,
+    history_service: TagHistoryService = Depends(get_tag_history_service),
+    pagination=Depends(PaginationArgs),
+    filtering=Depends(FilteringArgs),
+    sorting=Depends(SortingArgs),
+) -> PaginatedTagHistoryList:
+    query = {"term": {"id": str(tag_id)}}
+    count, limit, offset, res = await history_service.list(
+        pagination, filtering, sorting, filter_deleted=False, extra_filter=query
+    )
+    ret = [TagHistory(**i["_source"]) for i in res]
+    return PaginatedTagHistoryList(limit=limit, offset=offset, total=count, items=ret)
