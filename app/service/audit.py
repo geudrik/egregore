6c685f2d2e12b94a16cf83165a0a26476e59600c
Pat Litke <2020115+geudrik@users.noreply.github.com>
2024-05-30 17:57:41 -0400

    Add in metrics agg for audit log

--- app/service/audit.py
+++ app/service/audit.py
@@ -50,3 +50,69 @@ class AuditService(BaseService):
         }
         logger.debug("Adding new Audit entry", **body)
         await self._index(body=body)
+
+    async def metrics(self, bucket_size=100) -> dict:
+        """Agg our audit logs"""
+        body = {
+            "size": 0,
+            "aggs": {
+                "unique_components": {
+                    "terms": {"field": "component", "size": bucket_size},
+                    "aggs": {
+                        "unique_actions": {
+                            "terms": {"field": "action", "size": bucket_size},
+                            "aggs": {
+                                "unique_sub_components": {
+                                    "terms": {"field": "subcomponent", "size": bucket_size},
+                                    "aggs": {
+                                        "unique_sub_component_actions": {
+                                            "terms": {"field": "subcomponent_action", "size": bucket_size},
+                                            "aggs": {"unique_docs": {"cardinality": {"field": "_id"}}},
+                                        }
+                                    },
+                                }
+                            },
+                        }
+                    },
+                }
+            },
+        }
+
+        aggs = await self.client.search(index=self.index_name_read, body=body)
+
+        summary = {"components": []}
+
+        for component_bucket in aggs["aggregations"]["unique_components"]["buckets"]:
+            component = component_bucket["key"]
+            summary["components"].append(
+                {"count": component_bucket["doc_count"], "component": component, "actions": []}
+            )
+
+            for action_bucket in component_bucket["unique_actions"]["buckets"]:
+                action = action_bucket["key"]
+                summary["components"][-1]["actions"].append(
+                    {"count": action_bucket["doc_count"], "action": action, "subcomponents": []}
+                )
+
+                for sub_component_bucket in action_bucket["unique_sub_components"]["buckets"]:
+                    sub_component = sub_component_bucket["key"]
+                    summary["components"][-1]["actions"][-1]["subcomponents"].append(
+                        {
+                            "count": sub_component_bucket["doc_count"],
+                            "name": sub_component,
+                            "actions": [],
+                        }
+                    )
+
+                    for sub_component_action_bucket in sub_component_bucket["unique_sub_component_actions"]["buckets"]:
+                        sub_component_action = sub_component_action_bucket["key"]
+                        unique_count = sub_component_action_bucket["unique_docs"]["value"]
+
+                        summary["components"][-1]["actions"][-1]["subcomponents"][-1]["actions"].append(
+                            {
+                                "action": sub_component_action,
+                                "count": unique_count,
+                            }
+                        )
+
+        return summary
